330d8ee51be5d092237bb1aeb5ab23a774081bcb
Anjali <post2anjali@gmail.com>
2024-11-05 16:31:26 +0000

    committed RandomAccessQueue

---
+++ README.md
@@ -0,0 +1,19 @@
+# RandomAccessQueue java
+
+This is a free to reuse - RandomAccessQueue with constant time (O(1)) addition, removal and random access.
+Please also have a look at the license file.
+
+If you find it is useful for you and the community and more customised java data structures should be developed, please consider hitting that sponsorship button on the right!
+
+## How to use it?
+
+**You will need**
+
+- jdk 1.8
+
+
+**Queue has the ability to add at end and remove from top**
+
+-  Please let me know if you need more utilities for this queue, I can provide more versions of it.
+
+
---
+++ src/com/softwaretunnel/queues/RandomAccessQueue.java
@@ -0,0 +1,34 @@
+package com.softwaretunnel.queues;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author tunnels4u (Anjali)
+ **/
+public class RandomAccessQueue<E> {
+
+	private List<E> list = new ArrayList<E>();
+	private ArrayDeque<E> deQueue = new ArrayDeque<E>();
+
+	public void addLast(E e) {
+		deQueue.addLast(e);
+		list.add(e);
+	}
+
+	public E removeFirst() {
+		E e = deQueue.removeFirst();
+		shuffleList();
+		return e;
+	}
+
+	public E get(int index) {
+		return (E) list.get(index);
+	}
+
+	private void shuffleList() {
+		list = (List<E>) list.subList(1, list.size());// O(1) time
+	}
+
+}
