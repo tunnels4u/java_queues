75bfeb8cdbad508ff2bea0b1478e21e46468a51c
anjali <anjali@localhost>
2024-12-20 14:14:39 +0000

    Newer/better version

--- src/com/softwaretunnel/queues/RandomAccessQueue.java
+++ src/com/softwaretunnel/queues/RandomAccessQueue.java
@@ -5,42 +5,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Author tunnels4u (Anjali) 
- * Queue with the ability to add at end, remove from
+ * Author tunnels4u (Anjali) Queue with the ability to add at end, remove from
  * top and random access in constant time
  **/
 public class RandomAccessQueue<E> {
 
 	private List<E> list = new ArrayList<E>();
-	private ArrayDeque<E> deQueue = new ArrayDeque<E>();
+	private int indexOffset = 0;
 
 	public void addLast(E e) {
-		deQueue.addLast(e);
 		list.add(e);
 	}
 
 	public E removeFirst() {
-		E e = deQueue.removeFirst();
-		shuffleList();
+		E e = this.get(0);
+		shuffleListIndex();
 		return e;
 	}
 
 	public E get(int index) {
-		return (E) list.get(index);
+		return (E) list.get((index + indexOffset));
 	}
 
-	private void shuffleList() {
+	private void shuffleListIndex() {
 
 		int listSize = list.size();
 		if (listSize > 1) {
-			list = (List<E>) list.subList(1, listSize);// O(1) time
+			indexOffset++;
 		} else {
 			list.remove(0);
 		}
 	}
 
 	public int size() {
-		return list.size();
+		return (list.size() - indexOffset);
 	}
 
 }
